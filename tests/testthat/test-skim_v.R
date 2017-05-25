8790f2abdf734b20b7ba628741b1d5ba8608acc7
Michael Quinn <michael.quinn@aya.yale.edu>
2017-05-25 16:19:26 -0700

    Clean up after commit.

--- tests/testthat/test-skim_v.R
+++ tests/testthat/test-skim_v.R
@@ -9,12 +9,12 @@ correct <- tibble::tribble(
   "numeric", "complete",     NA,  32,
   "numeric",        "n",     NA,  32,
   "numeric",     "mean",     NA,  mean(mtcars$mpg),
-  "numeric",       "q1",     NA,  quantile(mtcars$mpg, probs = .25, names = F),
-  "numeric",       "q3",     NA,  quantile(mtcars$mpg, probs = .75, names = F),
   "numeric",       "sd",     NA,  sd(mtcars$mpg),
+  "numeric",      "min",     NA,  min(mtcars$mpg),
+  "numeric",       "q1",     NA,  quantile(mtcars$mpg, probs = .25, names = F),
   "numeric",   "median",     NA,  median(mtcars$mpg),
-  "numeric",      "max",     NA,  max(mtcars$mpg),
-  "numeric",      "min",     NA,  min(mtcars$mpg))
+  "numeric",       "q3",     NA,  quantile(mtcars$mpg, probs = .75, names = F),
+  "numeric",      "max",     NA,  max(mtcars$mpg))
 
 test_that("skim_v returns expected response for numeric vectors", {
   input <- skim_v(mtcars$mpg)
