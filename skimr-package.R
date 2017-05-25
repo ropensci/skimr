9f8dd2a402407632c146c46324e6d04ee0e52288
Hao Zhu <haozhu233@gmail.com>
2017-05-25 12:17:24 -0700

    add some basic files

---
+++ skimr-package.R
@@ -0,0 +1,12 @@
+#' skimr
+#'
+#' @importFrom dplyr group_by
+#' @importFrom magrittr %>%
+#' @name skimr-package
+#' @aliases skimr
+#' @docType package
+#' @keywords package
+NULL
+
+#' @export
+magrittr::`%>%`
