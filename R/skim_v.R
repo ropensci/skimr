57f6416737180d8beaed476524bc920b2859fff7
Elin Waring <elin.waring@gmail.com>
2017-05-26 15:40:32 -0700

    Add some date handling.

--- R/skim_v.R
+++ R/skim_v.R
@@ -108,6 +108,22 @@ complex_funs <- list(
   n = length
 )
 
+#' @describeIn skim_v Calculate summary statistics for logical vectors
+#' @export
+skim_v.Date <- function(x, FUNS = date_funs) {
+  skim_v_(x, FUNS)
+}
+
+date_funs <- list(
+  missing = n_missing,
+  complete = complete,
+  n = length,
+  min = purrr::partial(min, na.rm = TRUE),
+  max = purrr::partial(max, na.rm = TRUE),
+  median = purrr::partial(median, na.rm = TRUE),
+  n_unique = purrr::compose(length, unique)
+)
+
 
 #' @describeIn skim_v Default method for calculating summary statistics
 #' @export
