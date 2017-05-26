d9dca48db6a85749bb7b11a9309eea41d0569ab9
Michael Quinn <michael.quinn@aya.yale.edu>
2017-05-26 15:14:45 -0700

    Add a test for a complete data frame.

--- tests/testthat/test-skim.R
+++ tests/testthat/test-skim.R
@@ -2,21 +2,37 @@ context("Skimming a data.frame")
 
 # Target output -----------------------------------------------------------
 
-skim_chickwts <- data.frame(
-  var = rep(c("weight", "feed"), times = c(4, 8)),
-  type = rep(c("dbl", "factor"), times = c(4, 8)),
-  stat = c("mean", "median", "complete", "missing", rep("count", 6), "complete",
-    "missing"),
-  levels = c(rep(NA, 4), "casein", "horsebean", "linseed", "meatmeal",
-    "soybean", "sunflower", NA, NA),
-  values = c(mean(chickwts$weight), median(chickwts$weight),
-    length(chickwts$weight), 0, as.numeric(table(chickwts$feed)),
-    length(chickwts$feed), 0)
+correct <- tibble::tribble(
+  ~var,     ~type,     ~stat,      ~level,       ~value,
+  "weight", "numeric", "missing",  ".all",       0,
+  "weight", "numeric", "complete", ".all",       71,
+  "weight", "numeric", "n",        ".all",       71,
+  "weight", "numeric", "mean",     ".all",       mean(chickwts$weight),
+  "weight", "numeric", "sd",       ".all",       sd(chickwts$weight),
+  "weight", "numeric", "min",      ".all",       108.000,
+  "weight", "numeric", "median",   ".all",       258.000,
+  "weight", "numeric", "quantile",  "25%",       204.500,
+  "weight", "numeric", "quantile",  "75%",       323.500,
+  "weight", "numeric", "max",      ".all",       423.000,
+  "feed",   "factor",  "missing",   ".all",      0.0000,
+  "feed",   "factor",  "complete",  ".all",      71.0000,
+  "feed",   "factor",  "n",         ".all",      71.0000,
+  "feed",   "factor",  "count",     "casein",    12.0000,
+  "feed",   "factor",  "count",     "horsebean", 10.0000,
+  "feed",   "factor",  "count",     "linseed",   12.0000,
+  "feed",   "factor",  "count",     "meatmeal",  11.0000,
+  "feed",   "factor",  "count",     "soybean",   14.0000,
+  "feed",   "factor",  "count",     "sunflower", 12.0000,
+  "feed",   "factor",  "count",     NA,      0.0000,
+  "feed",   "factor",  "n_unique",  ".all",   6.0000
 )
-
-class(skim_chickwts) <- c("skim_tbl", "data.frame")
-
+  
+class(correct) <- c("skim_df", class(correct))
 
 # Begin tests -------------------------------------------------------------
 
+test_that("Skimming a data frame works as expected", {
+  input <- skim(chickwts)
+  expect_identical(input, correct)
+})
 
