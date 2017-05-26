1abc376fbcf70f30c3ef3b01138b03aaaf7b81e0
jules32 <jstewart@nceas.ucsb.edu>
2017-05-26 11:36:50 -0700

    fix `ERROR: The build directory does not contain a DESCRIPTION file so cannot be built as a package.` by reducing description field to 1 line

--- R/skimr-package.R
+++ R/skimr-package.R
@@ -1,5 +1,5 @@
 #' skimr
-#'
+#'  It provides functionality to deal with summary statistics iteratively and interactively as part of a pipeline. The #'returned skimr object is subsettable and offers a human readable output.
 #' @name skimr-package
 #' @aliases skimr
 #' @docType package
