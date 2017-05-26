3eca757d7f965cd21fe9baa2f7e59a4e268a68e6
Michael Quinn <michael.quinn@aya.yale.edu>
2017-05-26 16:55:39 -0700

    Clean up unnecessary argument

--- R/skim.R
+++ R/skim.R
@@ -11,7 +11,7 @@ skim <- function(.data) {
 
 #'@export
 
-skim.data.frame <- function(.data, ...) {
+skim.data.frame <- function(.data) {
   rows <- purrr::map(.data, skim_v)
   combined <- dplyr::bind_rows(rows, .id = "var")
   return(structure(combined, class = c("skim_df", class(combined))))
