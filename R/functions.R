37a45334ed52c5ae2f249b0b48d2059023726985
Michael Quinn <michael.quinn@aya.yale.edu>
2017-05-26 16:03:01 -0700

    Move over dispatch to a group of functions by type. Allow for setting new types and adding more functions to a type's set of summarizers.

--- R/functions.R
+++ R/functions.R
@@ -1,1 +1,111 @@
-# Tools for setting up groups of functions for dispatch
+#' @include stats.R
+NULL
+
+
+# Default summarizing functions for each type
+
+numeric_funs <- list(
+  missing = n_missing,
+  complete = complete,
+  n = length,
+  mean = purrr::partial(mean, na.rm = TRUE),
+  sd = purrr::partial(sd, na.rm = TRUE),
+  min = purrr::partial(min, na.rm = TRUE),
+  median = purrr::partial(median, na.rm = TRUE),
+  quantile = purrr::partial(quantile, probs = c(.25, .75), na.rm = TRUE),
+  max = purrr::partial(max, na.rm = TRUE),
+  hist = inline_hist
+)
+
+factor_funs <- list(
+  missing = n_missing,
+  complete = complete,
+  n = length,
+  count = purrr::partial(table, useNA = "always"),
+  n_unique = purrr::compose(length, levels)
+)
+
+character_funs <- list (
+  missing  = n_missing,
+  complete = complete,
+  n = length,
+  min = min_char,
+  max = max_char,
+  empty = n_empty,
+  n_unique = purrr::compose(length, unique)
+)
+
+logical_funs <- list(
+  missing = n_missing,
+  complete = complete,
+  n = length,
+  count = purrr::partial(table, useNA = "always"),
+  mean = purrr::partial(mean, na.rm = TRUE)
+)
+
+integer_funs <- numeric_funs
+
+.summary_functions_default <- list(
+  numeric = numeric_funs,
+  integer = integer_funs,
+  factor = factor_funs,
+  ordered = factor_funs,
+  character = character_funs,
+  logical = logical_funs
+)
+
+.summary_functions <- .summary_functions_default
+
+
+#' Set or add the summary functions for a particular type of data
+#' 
+#' @param ... A list of functions, with an argument name that matches a
+#'   particular data type.
+#' @param append Whether the provided functions should be in addition to the
+#'   defaults already in skim.
+#' @param reset Whether to revert to the original defaults.
+#' @export
+
+skim_with <- function(..., append = TRUE, reset = FALSE) {
+  if (reset) {
+    .summary_functions <- .summary_functions_default
+  } else {
+    funs < - list(...)
+    all <- purrr::map2(names(funs), funs, set_functions, append)
+  }
+}
+
+set_functions <- function(type, functions, append) {
+  new <- type %in% names(.summary_functions)
+  
+  if (new) {
+    message("Adding new summary functions for type:", type)
+  } else if (append) {
+    old <- .summary_functions[[type]]
+    .summary_functions[[type]] <- c(old, functions)
+  } else {
+    .summary_functions[[type]] <- functions
+  }
+}
+
+
+#' Show summary functions currently used, by column type
+#' 
+#' @return Nothing. \code{invisible()}
+#' @export
+
+show_skimmers <- function() {
+  purrr::map(.summary_functions, names)
+}
+
+
+# A method for getting a set of summary functions, by type
+#
+# @param type The type of summary functions to extract
+# @return A list of summary functions
+# @keywords internal
+# @export
+
+get_funs <- function(type) {
+  .summary_functions[[type]]
+}
