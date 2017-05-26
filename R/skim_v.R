2b51952c18c1d155e183fcd5d53f8cb7edfd09b3
Elin Waring <elin.waring@gmail.com>
2017-05-26 07:58:39 -0700

    Concepts for factor and character.

--- R/skim_v.R
+++ R/skim_v.R
@@ -25,6 +25,8 @@ skim_v.numeric <- function(x, FUNS = numeric_funs) {
   return(out)
 }
 
+
+
 numeric_funs <- list(
   missing = purrr::compose(sum, is.na),
   complete = purrr::compose(sum, `!`, is.na),
@@ -37,3 +39,34 @@ numeric_funs <- list(
   max = max,
   min = min
 )
+
+
+skim_v.factor <- function(x) {
+  t<-table(x, useNA = "always")
+  t<-tibble::as.tibble(t)
+  colnames(t)<-c("level", "value")
+  t$type <- "factor"
+  t$stat <- "count"
+  out <- t[c("type", "stat", "level", "value")]
+  return(out)
+}
+
+skim_v.character <- function(x, FUNS = character_funs) {
+  stats <- names(FUNS)
+  value <- purrr::map_dbl(FUNS, ~.x(x))
+  out <- tibble::tibble(type = class(x), 
+                        stat = stats,
+                        level = NA, 
+                        value = value)
+  return(out)
+}
+
+character_funs <- list (
+  n = length,
+  max = purrr::compose(max, nchar),
+  min = purrr::compose(min, nchar),
+  #blank    = ,
+  missing  = purrr::compose(sum, is.na),
+  n_unique = purrr::compose(length, unique)
+)
+
