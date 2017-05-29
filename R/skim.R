bae3f29dba3d8c3bfbe71af50c716a312bc5298f
GShotwell <gordon.shotwell@gmaill.com>
2017-05-29 14:21:09 -0300

    Added handling of grouped data frames
    Print method does not work properly

--- R/skim.R
+++ R/skim.R
@@ -12,7 +12,39 @@ skim <- function(.data) {
 #'@export
 
 skim.data.frame <- function(.data) {
-  rows <- purrr::map(.data, skim_v)
-  combined <- dplyr::bind_rows(rows, .id = "var")
+  if (dplyr::is.grouped_df(.data)) {
+    skim_df <- .data %>% 
+      tidyr::nest() %>% 
+      dplyr::mutate(stats = purrr::map(data, skim)) %>% 
+      dplyr::mutate(stats = purrr::map(stats, 
+                                       append_group_vars, 
+                                       groups = dplyr::groups(.data)))
+    combined <- dplyr::bind_rows(skim_df$stats)
+  } else {
+    rows <- purrr::map(.data, skim_v)
+    combined <- dplyr::bind_rows(rows, .id = "var")
+  }
   return(structure(combined, class = c("skim_df", class(combined))))
+}
+
+
+#' Append Grouping Variables
+#'
+#' @param df 
+#' A \code{skim_df}
+#' @param groups 
+#' The grouping variables of the data frame to be summarized
+#'
+#' @return
+#' A data frame with the grouping variables in new columns, labeled by "group_var_1", 
+#' "group_var_2" etc. 
+#'
+#' @examples
+append_group_vars <- function(df, groups){
+  group_char <- dplyr::data_frame(x = as.character(groups)) %>% 
+    t() %>% 
+    dplyr::as_data_frame()
+  group_var_df <- purrr::map(1:nrow(df), ~group_char) %>% dplyr::bind_rows()
+  names(group_var_df) <- paste0("group_var_", seq_along(groups))
+  dplyr::bind_cols(df, group_var_df)
 }
