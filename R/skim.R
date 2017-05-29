f145751a20a6ebcf198c17e593af3ffd8a7ecab4
GShotwell <gordon.shotwell@gmaill.com>
2017-05-29 14:50:53 -0300

    Added handling of grouped data frames
    Print method does not work properly
    Printed out value of grouping variable
    
    
    Included grouping variable value

--- R/skim.R
+++ R/skim.R
@@ -12,7 +12,44 @@ skim <- function(.data) {
 #'@export
 
 skim.data.frame <- function(.data) {
-  rows <- purrr::map(.data, skim_v)
-  combined <- dplyr::bind_rows(rows, .id = "var")
+  if (dplyr::is.grouped_df(.data)) {
+    nested_df <- .data %>% 
+      tidyr::nest() 
+    groups <-  as.character(dplyr::groups(.data))
+    group_values <- by_row(nested_df[, groups], ~combine(.))
+    
+    skim_df <- nested_df %>% 
+      dplyr::mutate(stats = purrr::map(data, skim)) %>% 
+      dplyr::mutate(stats = purrr::map2(stats, 
+                                        group_values$.out,
+                                       ~append_group_vars(.x, .y, groups = groups)))
+    
+    combined <- dplyr::bind_rows(skim_df$stats)
+  } else {
+    rows <- purrr::map(.data, skim_v)
+    combined <- dplyr::bind_rows(rows, .id = "var")
+  }
   return(structure(combined, class = c("skim_df", class(combined))))
+}
+
+
+#' Append Grouping Variables
+#'
+#' @param df 
+#' A \code{skim_df}
+#' @param groups 
+#' The grouping variables of the data frame to be summarized
+#'
+#' @return
+#' A data frame with the grouping variables in new columns, labeled by "group_var_1", 
+#' "group_var_2" etc. 
+#'
+#' @examples
+append_group_vars <- function(df, val, groups){
+  group_char <- dplyr::data_frame(x = val) %>% 
+    t() %>% 
+    dplyr::as_data_frame()
+  group_var_df <- purrr::map(1:nrow(df), ~group_char) %>% dplyr::bind_rows()
+  names(group_var_df) <- paste0("group_var_", groups)
+  dplyr::bind_cols(df, group_var_df)
 }
