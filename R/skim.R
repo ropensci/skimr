73783518e7aceec37f2c56c8e46ca12e679cf3c0
GShotwell <gordon.shotwell@gmaill.com>
2017-05-31 15:43:56 -0300

    Preserved grouping variables in skim.grouped_df()
    This is to match the output of dplyr::summarize().

--- R/skim.R
+++ R/skim.R
@@ -13,26 +13,39 @@ skim <- function(.data) {
 #'@export
 
 skim.data.frame <- function(.data) {
-  if (dplyr::is.grouped_df(.data)) {
-    nested_df <- .data %>% 
-      tidyr::nest() 
-    groups <-  as.character(dplyr::groups(.data))
-    group_values <- purrr::by_row(nested_df[, groups], ~dplyr::combine(.))
-    
-    skim_df <- nested_df %>% 
-      dplyr::mutate(stats = purrr::map(data, skim)) %>% 
-      dplyr::mutate(stats = purrr::map2(stats, 
-                                        group_values$.out,
-                                       ~append_group_vars(.x, .y, groups = groups)))
-    
-    combined <- dplyr::bind_rows(skim_df$stats)
-  } else {
-    rows <- purrr::map(.data, skim_v)
-    combined <- dplyr::bind_rows(rows, .id = "var")
-  }
+  rows <- purrr::map(.data, skim_v)
+  combined <- dplyr::bind_rows(rows, .id = "var")
   return(structure(combined, class = c("skim_df", class(combined))))
 }
 
+#' Title
+#'
+#' @param .data 
+#'
+#' @return
+#' A grouped data frame
+#' @export
+#'
+#' @examples
+#' mtcars %>% group_by(cyl, gear) %>% skim()
+#' 
+skim.grouped_df <- function(.data){
+  nested_df <- .data %>%   
+    tidyr::nest() 
+  groups <-  as.character(dplyr::groups(.data))
+  
+  l <- split(nested_df[, groups], factor(1:nrow(nested_df))) 
+  l <- purrr::map( l, ~dplyr::combine(.))
+  
+  skim_df <- nested_df %>% 
+    dplyr::mutate(stats = purrr::map(data, skim)) %>% 
+    dplyr::mutate(stats = purrr::map2(stats, 
+                                      l,
+                                      ~append_group_vars(.x, .y, groups = groups)))
+  combined <- dplyr::bind_rows(skim_df$stats) %>% 
+    dplyr::group_by_(.dots = groups)
+  return(structure(combined, class = c("skim_df", class(combined))))
+}
 
 #' Append Grouping Variables
 #'
@@ -42,15 +55,14 @@ skim.data.frame <- function(.data) {
 #' The grouping variables of the data frame to be summarized
 #'
 #' @return
-#' A data frame with the grouping variables in new columns, labeled by "group_var_1", 
-#' "group_var_2" etc. 
+#' A data frame with the grouping variables in new columns
 #'
 #' @examples
 append_group_vars <- function(df, val, groups){
-  group_char <- dplyr::data_frame(x = val) %>% 
+  group_val <- dplyr::data_frame(x = val) %>% 
     t() %>% 
     dplyr::as_data_frame()
-  group_var_df <- purrr::map(1:nrow(df), ~group_char) %>% dplyr::bind_rows()
-  names(group_var_df) <- paste0("group_var_", groups)
-  dplyr::bind_cols(df, group_var_df)
+  group_var_df <- purrr::map(1:nrow(df), ~group_val) %>% dplyr::bind_rows()
+  names(group_var_df) <-  groups
+  dplyr::bind_cols(group_var_df, df)
 }
