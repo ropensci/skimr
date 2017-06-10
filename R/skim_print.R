552ce1ed3588c17136fade1eb72893d88810bab7
Elin Waring <elin.waring@gmail.com>
2017-06-10 11:38:14 -0400

    Basic concept for a more generic print. It works, but should be abstracted more.

--- R/skim_print.R
+++ R/skim_print.R
@@ -8,90 +8,72 @@
 #' @return A \code{skim_df} object
 #' @export
 skim_print <- function(x){
-  nums_dt <- x[x$type %in% c("numeric", "double", "integer"),]
-  fcts_dt <- x[x$type == "factor",]
-  chars_dt <- x[x$type == "character",]
+ 
+  x$stat <- ifelse(x$level %in% c(".all") | x$stat == "hist" | x$stat == "count", x$stat,  paste(x$stat, x$level))
+  return_list <- list()
+  types <- unique(x$type)
+  types_custom <- c("numeric", "double", "integer", "factor", "character")
+  types_other <- subset(types, !types %in% types_custom)
   
-  nums <- NULL
-  fcts <- NULL
-  chars <- NULL
-  
-  if (nrow(nums_dt) > 0) {
-    nums <- formatnum(nums_dt)
+  y <- x %>% filter(type %in%  c("numeric", "double", "integer")) 
+  if  (nrow(y) > 0){
+    order<-unique(y$stat)
+    type <- "Numeric"
+    histograms <- y %>% dplyr::filter(stat == "hist")
+    y <- y %>% dplyr::select(var, stat, value) %>% tidyr::spread( stat, value)
+    calculated <- c("max",  "mean", "median", "min", "quantile 25%", "quantile 75%", "sd")
+    # Not a real solution
+    y[calculated] <- round(y[calculated], digits = 2)
+    y$hist <- histograms$level
+    
+    y <- y[c("var", order)]
+    return_list[[type]] <- y
+    attr(return_list[[type]], "subtibble_title") <- paste(type, "Variables\n")
   }
-  if (nrow(fcts_dt) > 0) {
-    fcts <- formatfct(fcts_dt)
+
+  y <- x %>% filter(type == "factor")
+  if  (nrow(y) > 0){
+    type = "Factor"
+    counts <- y %>% dplyr::filter (stat == "count") 
+    counts <- paste(paste(counts$var, counts$level, counts$value, sep = ":"), collapse = " ")
+    
+    y <- y %>% dplyr::filter(stat != "count") %>% dplyr::select(var, stat, value) %>% tidyr::spread( stat, value)
+    y <- tibble::add_column(y, counts)
+    return_list[[type]] <- y
+    attr(return_list[[type]], "subtibble_title") <- paste(type, "Variables\n")
   }
-  if (nrow(chars_dt) > 0) {
-    chars <- formatchars(chars_dt)
+
+  y <- x %>% filter(type == "character")
+  if  (nrow(y) > 0){
+    type = "Character"
+    order<-unique(y$stat)
+    y <- y %>% dplyr::select(var, stat, value) %>% tidyr::spread( stat, value)
+    y <- y[c("var", order)]
+    return_list[[type]] <- y
+    attr(return_list[[type]], "subtibble_title") <- paste(type, "Variables\n")
   }
-  return(list(numeric = nums, factors = fcts, characters=chars))
-}
-
-formatnum <- function(x) {
-  tmp1 <- dplyr::mutate(x, stat = ifelse(stat == "quantile", 
-                               paste(level, stat), stat))
-  tmp1 <- dplyr::select(dplyr::filter(tmp1, stat != "hist"), -level)
-  tmp1 <- dplyr::mutate(tmp1, stat = factor(stat, levels = c(
-    "missing", "complete", "n", "mean", "sd", "min", "25% quantile", "median",
-    "75% quantile", "max", "hist"
-  )))
-  tmp1 <- tidyr::spread(tmp1, stat, value)
   
-  tmp2 <- dplyr::mutate(dplyr::filter(x, stat == "hist"), value = level)
-  tmp2 <- tidyr::spread(dplyr::select(tmp2, -level), stat, value)
-  
-  tmp <- dplyr::left_join(tmp1, tmp2, by = c("var", "type"))
-  return(tmp)
-}
-
-formatfct <- function(x) {
-  tmp <- x[x$level == ".all",]
-  tmp <- tmp[complete.cases(tmp),]
-  tmp <- tidyr::spread(dplyr::select(tmp, -level), stat, value)
-  tmp1 <- x[x$stat == "count",]
-
-  wide <- tibble::data_frame()
-  for (i in unique(x$var)) {
-    tmp2 <- tmp1[tmp1$var == i,]
-    tmp2$stat <- paste0(tmp2$level, ": ", tmp2$value)
-    wide <- rbind(wide, tibble::data_frame(var = i, 
-                                   type = "factor", 
-                                   stat = paste0(tmp2$stat, collapse = " ")))
+  y <- x %>% filter(type %in% types_other)
+  if (nrow(y) > 0) {
+    for (i in 1:length(types_other)){
+      z <- x %>% filter(type == types_other[i])
+        order<-unique(z$stat)
+        z <- z %>% dplyr::select(var, stat, value) %>% tidyr::spread( stat, value)
+        z <- z[c("var", order)]
+        return_list[[types_other[i]]] <- z 
+        attr(return_list[[type]], "subtibble_title") <- paste(type, "Variables\n")
+    }
   }
-  tmp <- dplyr::left_join(tmp, wide, by = c("var", "type"))
-  
-  return(tmp)
-}
 
-formatchars <- function(x) {
-  tmp1 <- dplyr::mutate(x, stat = ifelse(stat == "quantile", 
-                                         paste(level, stat), stat))
-  tmp1 <- dplyr::select(dplyr::filter(tmp1, stat != "hist"), -level)
-  tmp1 <- dplyr::mutate(tmp1, stat = factor(stat, levels = c(
-    "complete","missing", "empty", "n", "min","max", "n_unique" 
-  )))
-  tmp1 <- tidyr::spread(tmp1, stat, value)
-  
-  return(tmp1)
+  return(return_list)   
 }
 
 #' @export
 print.skim_df <- function(x, ...) {
-  wide_values <- skim_print(x)
-  
-  if (! is.null(wide_values$numeric)) {
-    cat("Numeric Variables\n")
-    print(wide_values$numeric)
-  }
-  
-  if (! is.null(wide_values$characters)) {
-    cat("\nCharacter Variables\n")
-    print(wide_values$characters)
-  }
-  
-  if (! is.null(wide_values$factors)) {
-    cat("\nFactor Variables\n")
-    print(wide_values$factors)
-  }
+  lapply(skim_print(x),print_results)
+}
+
+print_results<-function(subtibble){
+  cat(attr(subtibble, "subtibble_title"))
+  print(subtibble)
 }
