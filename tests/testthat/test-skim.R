1621e27c7383290a0a60b6b788ed7aabd86717cf
Elin Waring <elin.waring@gmail.com>
2017-06-17 21:22:12 -0400

    Fix the test data reference.

--- tests/testthat/test-skim.R
+++ tests/testthat/test-skim.R
@@ -64,9 +64,9 @@ test_that("Using skim_tee prints out the object", {
 })
 
 test_that("Skimming a grouped data frame works as expected", {
-  sysfile <- system.file("test/testthat/skim_output/","group_df.RDS",  package="skimr")
+  sysfile <- system.file("tests/testthat/skim_output/","group_df.RDS",  package="skimr")
   if (sysfile == ""){
-    correct <- readRDS(paste0(getwd(),"/skim_output/group_df.RDS"))
+    correct <- readRDS("tests/testthat/skim_output/group_df.RDS")
   } else {
     correct <- readRDS(sysfile)
   }
