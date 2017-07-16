c9b5ab92c0f0872fa9388e400d97813cdd31a678
Michael Quinn <michael.quinn@aya.yale.edu>
2017-07-16 15:29:26 -0700

    Fixes #116
    Give users the option of selecting which summary functions are returned by show_skimmers. It does this by accepting an argument for a data type/ class.
    
    For example skim_with("numeric") gives the names of functions used to summarize numeric data.

--- R/functions.R
+++ R/functions.R
@@ -83,12 +83,26 @@ skim_with_defaults <- function() {
 #' a list. The names of the values in the list match the names of the classes
 #' that have assigned sets of summary functions.
 #' 
+#' @param which A character vector. One or more of the classes whose summary
+#'  functions you wish to display.
 #' @return A list. The names of the list match the classes that have assigned
 #'  summary functions.
 #' @export
 
-show_skimmers <- function() {
-  lapply(functions$current, names)
+show_skimmers <- function(which = NULL) {
+  stopifnot(is.null(which) || is.character(which))
+  skimmers <- lapply(functions$current, names)
+  if (is.null(which)) {
+    skimmers
+  } else {
+    out <- skimmers[which]
+    missed <- is.na(names(out))
+    if (any(missed)) {
+      warning("Skim functions aren't defined for type(s): ",
+              paste(which[missed], collapse = ", "))
+    }
+    out[!missed]
+  }
 }
 
 
--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -8,12 +8,40 @@ test_that("show_skimmers() has a correct list of functions for a type", {
   identical(input, correct)
 })
 
-test_that("show_skimmers() has a correct list of types", {
-  correct <- c("numeric",   "integer",  "factor" ,   "ordered",   "character", "logical",   "complex",
-               "date",      "Date",      "ts", "POSIXct" )
+test_that("show_skimmers() has a correct list of default types", {
+  correct <- c("numeric", "integer", "factor" , "character", "logical",
+               "complex", "date", "Date", "ts", "POSIXct" )
   skimmers <- show_skimmers()
   input <- names(skimmers)
   identical(input, correct)
+})
+
+test_that("show_skimmers() lets you pick which type you want returned", {
+  correct <- list(character = c("missing", "complete", "n", "min", "max",
+                                "empty", "n_unique"))
+  skimmers <- show_skimmers("character")
+  expect_identical(correct, skimmers)
+})
+
+test_that("show_skimmers() lets you pick which many types you want returned", {
+  correct <- list(numeric = c("missing", "complete", "n", "mean",  "sd", "min",
+                              "median", "quantile", "max", "hist"),
+                  character = c("missing",  "complete", "n", "min", "max",
+                                "empty", "n_unique"))
+  skimmers <- show_skimmers(c("numeric", "character"))
+  expect_identical(correct, skimmers)
+})
+
+test_that("show_skimmers() throws a warning when given an unassigned type", {
+  expect_warning(skimmers <- show_skimmers("banana"), "aren't defined")
+  expect_identical(skimmers, setNames(list(), character(0)))
+})
+
+test_that("show_skimmers() returns something if given an unassigned type", {
+  expect_warning(skimmers <- show_skimmers(c("character", "banana")))
+  correct <- list(character = c("missing", "complete", "n", "min", "max",
+                                "empty", "n_unique"))
+  expect_identical(skimmers, correct)
 })
 
 test_that("Skimmer list is updated correctly when changing functions", {
@@ -27,9 +55,9 @@ test_that("Skimmer list is updated correctly when changing functions", {
 })
 
 correct <- tibble::tribble(
-  ~type,          ~stat,  ~level,   ~value,
-  "numeric",      "iqr",  ".all",   IQR(iris$Sepal.Length),
-  "numeric", "quantile",   "99%",   7.7
+  ~type,     ~stat,      ~level,  ~value,
+  "numeric", "iqr",      ".all",  IQR(iris$Sepal.Length),
+  "numeric", "quantile", "99%",   7.7
 )
 
 test_that("Skimming functions can be changed for different types", {
@@ -44,20 +72,20 @@ test_that("Skimming functions can be changed for different types", {
 })
 
 correct <- tibble::tribble(
-  ~type,          ~stat,    ~level,   ~value,
-  "numeric",      "missing", ".all",   0,
-  "numeric",      "complete",".all",   150,
-  "numeric",      "n",       ".all",   150,
-  "numeric",      "mean",    ".all",   mean(iris$Sepal.Length),
-  "numeric",      "sd",      ".all",   sd(iris$Sepal.Length),
-  "numeric",      "min",     ".all",   4.3,
-  "numeric",      "median",  ".all",   5.8,
-  "numeric",      "quantile","25%",    5.1,
-  "numeric",      "quantile","75%",    6.4,
-  "numeric",      "max",     ".all",   7.9,
-  "numeric",      "hist", "▂▇▅▇▆▆▅▂▂▂",0,              
-  "numeric",      "iqr",     ".all",   IQR(iris$Sepal.Length),
-  "numeric",      "quantile","99%",    7.7
+  ~type,          ~stat,      ~level,       ~value,
+  "numeric",      "missing",  ".all",       0,
+  "numeric",      "complete", ".all",       150,
+  "numeric",      "n",        ".all",       150,
+  "numeric",      "mean",     ".all",       mean(iris$Sepal.Length),
+  "numeric",      "sd",       ".all",       sd(iris$Sepal.Length),
+  "numeric",      "min",      ".all",       4.3,
+  "numeric",      "median",   ".all",       5.8,
+  "numeric",      "quantile", "25%",        5.1,
+  "numeric",      "quantile", "75%",        6.4,
+  "numeric",      "max",      ".all",       7.9,
+  "numeric",      "hist",     "▂▇▅▇▆▆▅▂▂▂", 0,
+  "numeric",      "iqr",      ".all",       IQR(iris$Sepal.Length),
+  "numeric",      "quantile", "99%",        7.7
 )
 
 test_that("Skimming functions can be appended.", {
@@ -73,8 +101,8 @@ test_that("Skimming functions can be appended.", {
 
 correct <- tibble::tribble(
   ~type,          ~stat,  ~level,   ~value,
-  "new_type",      "iqr",  ".all",   IQR(iris$Sepal.Length),
-  "new_type", "quantile",   "99%",   7.7
+  "new_type",      "iqr", ".all",   IQR(iris$Sepal.Length),
+  "new_type", "quantile",  "99%",   7.7
 )
 
 test_that("Skimming functions for new types can be added", {
@@ -90,10 +118,10 @@ test_that("Skimming functions for new types can be added", {
 })
 
 correct <- tibble::tribble(
-  ~type,          ~stat,  ~level,   ~value,
-  "new_type",      "iqr",  ".all",   IQR(iris$Sepal.Length),
-  "new_type", "quantile",   "99%",   7.7,
-  "new_type", "q2",   "99%",   7.7
+  ~type,          ~stat,      ~level,   ~value,
+  "new_type",     "iqr",      ".all",   IQR(iris$Sepal.Length),
+  "new_type",     "quantile", "99%",   7.7,
+  "new_type",     "q2",       "99%",   7.7
 )
 
 test_that("Set multiple sets of skimming functions", {
@@ -118,5 +146,3 @@ test_that("Throw errors when arguments are incorrect", {
   # Restore defaults
   skim_with_defaults()
 })
-
-skim_with_defaults()
