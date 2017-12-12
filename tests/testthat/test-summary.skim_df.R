9a258787e3ebd5a5395c8bc61cffec77542b4196
Connor Kirkpatrick <connor.kirkpatrick@hotmail.com>
2017-12-12 17:32:28 +0000

    Split out creation of test input objects

--- tests/testthat/test-summary.skim_df.R
+++ tests/testthat/test-summary.skim_df.R
@@ -2,22 +2,24 @@ context("Make and print a summary of a skim object")
 
 test_that("Summary creates the correct summary object", {
   # Test it creates the correct 4 parts
-  input <- summary(skim(iris))
-  expect_named(input, c("df_name", "n_rows", "n_cols", "type_frequencies"))
+  skim_input <- skim(iris)
+  summary_input <- summary(skim_input)
+  expect_named(summary_input, c("df_name", "n_rows", "n_cols", "type_frequencies"))
   
 })
 
 test_that("The summary print method prints the correct object", {
   # Test that the correct lines are output
-  input <- summary(skim(iris))
+  skim_input <- skim(iris)
+  summary_input <- summary(skim_input)
   
-  expect_output(print(input), "A skim object")
+  expect_output(print(summary_input), "A skim object")
   
-  expect_output(print(input), "Name: iris")
-  expect_output(print(input), "Number of Rows: 150")
-  expect_output(print(input), "Number of Columns: 5")
+  expect_output(print(summary_input), "Name: iris")
+  expect_output(print(summary_input), "Number of Rows: 150")
+  expect_output(print(summary_input), "Number of Columns: 5")
   
-  expect_output(print(input), "Column type frequency")
-  expect_output(print(input), "factor: 1")
-  expect_output(print(input), "numeric: 4")
+  expect_output(print(summary_input), "Column type frequency")
+  expect_output(print(summary_input), "factor: 1")
+  expect_output(print(summary_input), "numeric: 4")
 })
