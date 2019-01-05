4c2a84d4b9d480b4503883605abedb212a4647b9
Elin Waring <elin.waring@gmail.com>
2019-01-05 04:35:04 -0500

    Make classes match the old results exactly.

---
+++ R/filter.r
@@ -0,0 +1,12 @@
+#' Use dplyr verb filter on skim_df objects.
+#' 
+#' @seealso [`dplyr::filter()`]
+#' @inheritParams dplyr::filter
+#' @return  skim_df object coerced to a data frame.
+#' @export
+filter.skim_df <-function (.data, ..., .preserve = FALSE) {
+  .data <- as.data.frame(.data)
+  .data <- dplyr::filter(.data, ..., .preserve)
+  class(.data)  <- c("tbl_df",     "tbl", "data.frame")
+  .data
+}
