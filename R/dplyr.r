cc049a4fd344783dddd02c7ed697ef34cc308144
Elin Waring <elin.waring@gmail.com>
2019-01-12 10:59:29 -0500

    Concept for v2 dplyr verbs except select.

--- R/dplyr.r
+++ R/dplyr.r
@@ -3,12 +3,14 @@
 # #' @inheritParams dplyr::filter
 # #' @return  skim_df object coerced to a data frame.
 # #' @export
-# filter.skim_df <-function (.data, ..., .preserve = FALSE) {
-#   .data <- as.data.frame(.data)
-#   .data <- dplyr::filter(.data, ...)
-#   class(.data)  <- c("tbl_df", "tbl", "data.frame")
-#   .data
-# }
+filter.skim_df <-function (.data, ..., .preserve = FALSE) {
+   .data <- as.data.frame(.data)
+   .data <- dplyr::filter(.data, ...)
+   if (!("skim_df" %in% class(.data) )){
+     class(.data)  <- c(class(.data), "skim_df")
+   }
+   .data
+}
 #
 # #' Use dplyr verb select on skim_df objects.
 # #' #' @seealso [`dplyr::select()`]
@@ -18,7 +20,9 @@
 # select.skim_df <-function (.data, ...) {
 #   .data <- as.data.frame(.data)
 #   .data <- dplyr::select(.data, ...)
-#   class(.data)  <- c("tbl_df", "tbl", "data.frame")
+#    if (!("skim_df" %in% class(.data) )){
+#       class(.data)  <- c(class(.data), "skim_df")
+#    }
 #   .data
 # }
 #
@@ -27,33 +31,39 @@
 # #' @inheritParams dplyr::mutate
 # #' @return  skim_df object coerced to a data frame.
 # #' @export
-# mutate.skim_df <-function (.data, ...) {
-#   .data <- as.data.frame(.data)
-#   .data <- dplyr::mutate(.data, ...)
-#   class(.data)  <- c("tbl_df", "tbl", "data.frame")
-#   .data
-# }
+ mutate.skim_df <-function (.data, ...) {
+   .data <- as.data.frame(.data)
+   .data <- dplyr::mutate(.data, ...)
+    if (!("skim_df" %in% class(.data) )){
+       class(.data)  <- c(class(.data), "skim_df")
+    }
+   .data
+ }
 #
 # #' Use dplyr verb arrange on skim_df objects.
 # #' #' @seealso [`dplyr::arrange()`]
 # #' @inheritParams dplyr::arrange
 # #' @return  skim_df object coerced to a data frame.
 # #' @export
-# arrange.skim_df <-function (.data, ...) {
-#   .data <- as.data.frame(.data)
-#   .data <- dplyr::arrange(.data, ...)
-#   class(.data)  <- c("tbl_df", "tbl", "data.frame")
-#   .data
-# }
+ arrange.skim_df <-function (.data, ...) {
+   .data <- as.data.frame(.data)
+   .data <- dplyr::arrange(.data, ...)
+    if (!("skim_df" %in% class(.data) )){
+       class(.data)  <- c(class(.data), "skim_df")
+    }
+   .data
+ }
 #
 # #' Use dplyr verb slice on skim_df objects.
 # #' #' @seealso [`dplyr::slice()`]
 # #' @inheritParams dplyr::filter
 # #' @return  skim_df object coerced to a data frame.
 # #' @export
-# slice.skim_df <-function (.data, ..., .preserve = FALSE) {
-#   .data <- as.data.frame(.data)
-#   .data <- dplyr::slice(.data, ...)
-#   class(.data)  <- c("tbl_df", "tbl", "data.frame")
-#   .data
-# }
+ slice.skim_df <-function (.data, ..., .preserve = FALSE) {
+   .data <- as.data.frame(.data)
+   .data <- dplyr::slice(.data, ...)
+    if (!("skim_df" %in% class(.data) )){
+       class(.data)  <- c(class(.data), "skim_df")
+    }
+   .data
+ }
