18e8ca64977cb311df5af3630fcbb38594d0e257
Michael Quinn <michael.quinn@aya.yale.edu>
2019-01-26 20:28:05 -0800

    Create a single doc for deprecated functions.

--- R/reshape.R
+++ R/reshape.R
@@ -117,19 +117,6 @@ focus <- function(.data, ...) {
   dplyr::select(.data, "variable", "type", ...)
 }
 
-#' Skim to a wide data frame
-#' Deprecated
-#' @param .data A tibble, or an object that can be coerced into a tibble.
-#' @param ...  Columns to select for skimming. When none are provided, the
-#'   default is to skim all columns.
-#' @return A `skim_df` object, which also inherits the class(es) of the input
-#'   data. The result is usually a data frame or tibble.
-#' @export
-skim_to_wide<- function(.data, ...){
-  .Deprecated("skim()")
-  skim(.data, ...)
-}
-
 #' Skim results returned as a tidy long data frame with four columns:
 #' variable, type, stat and formatted.
 #' @param .data A tibble, or an object that can be coerced into a tibble.
@@ -146,13 +133,29 @@ to_long <- function( .data, ...){
                 -!!rlang::sym("type"), -!!rlang::sym("variable")) 
 }
 
-#' Skim to a list of data frames by type
-#' Deprecated
+#' Deprecated functions from skimr v1
+#' 
+#' Skimr used to offer functions that combined skimming with a secondary effect,
+#' like reshaping the data, building a list or printing the results. Some of
+#' these behaviors are no longer necessary. [skim()] always returns a wide
+#' data frame. Others have been replaced by functions that do a single thing.
+#' [partition()] creates a list-like object from a skimmed data frame.
+#' 
 #' @param .data A tibble, or an object that can be coerced into a tibble.
 #' @param ...  Columns to select for skimming. When none are provided, the
 #'   default is to skim all columns.
-#' @return A `skim_df` object, which also inherits the class(es) of the input
-#'   data. The result is usually a data frame or tibble.
+#' @return Either A `skim_df` or a `skim_list` object.
+#' @name deprecated-v1
+NULL
+
+#' @rdname deprecated-v1
+#' @export
+skim_to_wide<- function(.data, ...){
+  .Deprecated("skim()")
+  skim(.data, ...)
+}
+
+#' @rdname deprecated-v1
 #' @export
 skim_to_list<- function(.data, ...){
   .Deprecated("partition(skim())")
