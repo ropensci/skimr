19a683eb3c67f44fb4596d0050627c92131b0b41
Elin Waring <elin.waring@gmail.com>
2019-02-06 20:59:27 -0500

    Update tests, mainly for column order.

--- tests/testthat/test-reshape.R
+++ tests/testthat/test-reshape.R
@@ -90,8 +90,8 @@ test_that("Partition handles new columns", {
 
 test_that("focus() is identical to dplyr::select(data, type, variable, ...)", {
   skimmed <- skim(iris)
-  expected <- dplyr::select(skimmed, type, variable, missing)
-  expect_identical(focus(skimmed, missing), expected)
+  expected <- dplyr::select(skimmed, variable, type, missing)
+  expect_equal(focus(skimmed, missing), expected)
 })
 
 test_that("skim_to_wide() returns a deprecation warning", {
@@ -106,7 +106,7 @@ test_that("to_long() returns a long tidy data frame with 4 columns", {
   skimmed_long <- to_long(iris)
   # Statistics from the skim_df  with values of NA are not included
   expect_equal(nrow(skimmed_long), 50)
-  expect_equal(names(skimmed_long), c("type", "variable", "stat", "formatted"))
+  expect_equal(names(skimmed_long), c("variable", "type", "stat", "formatted"))
   expect_equal(length(unique(skimmed_long$stat)), 14)
   expect_equal(length(unique(skimmed_long$type)), 2)
   expect_equal(length(unique(skimmed_long$variable)), 5)
