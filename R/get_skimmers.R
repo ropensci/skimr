09e2cf050e1ef8af97a4dc49a361a37bc935aceb
Michael Quinn <michael.quinn@aya.yale.edu>
2019-02-10 17:08:18 -0800

    Expand functions for working with defaults
    - Add get_sfl for looking up sfls.
    - Separate getting names from getting funs
    - Separate getting a list, by type, back (get_default_skimmers) from just a list of functions.
    - Add a helper for modifying skimmers.

--- R/get_skimmers.R
+++ R/get_skimmers.R
@@ -23,9 +23,15 @@ NULL
 #'  - `ts`
 #'  - `AsIs`
 #'
-#' Call [get_default_skimmers()] to see the functions for each.
+#' Call [get_default_skimmers()] to see the functions for each. Call
+#' [get_default_skimmer_names()] to just see the names of these functions. Use
+#' [modify_default_skimmers()] for a method for changing the `skim_type` or
+#' functions for a default `sfl`. This is useful for creating new default
+#' `sfl`'s.
 #'
 #' @param column An atomic vector or list. A column from a data frame.
+#' @param skim_type A character scalar. The class of the object with default
+#'   skimmers.
 #' @return A `skim_function_list` object.
 #' @seealso [sfl()]
 #' @examples
@@ -60,6 +66,11 @@ NULL
 #'
 #'   # Or, in a local session, use `skim_with` to create a different `skim`.
 #'   new_skim <- skim_with(integer = skimr::get_skimmers.numeric())
+#'   
+#'   # To apply a set of skimmers from an old type to a new type
+#'   get_skimmers.new_type <- function(column) {
+#'     modify_default_skimmers("old_type", new_skim_type = "new_type")
+#'   }
 #' }
 #' @export
 get_skimmers <- function(column) {
@@ -68,7 +79,7 @@ get_skimmers <- function(column) {
 
 #' @export
 get_skimmers.default <- function(column) {
-  purrr::list_modify(get_skimmers(character()), skim_type = "default")
+  modify_default_skimmers("character", new_skim_type = "default")
 }
 
 #' @export
@@ -155,18 +166,12 @@ get_skimmers.Date <- function(column) {
 
 #' @export
 get_skimmers.POSIXct <- function(column) {
-  purrr::list_modify(
-    get_skimmers(structure(list(), class = "Date")),
-    skim_type = "POSIXct"
-  )
+  modify_default_skimmers("Date", new_skim_type = "POSIXct")
 }
 
 #' @export
 get_skimmers.difftime <- function(column) {
-  purrr::list_modify(
-    get_skimmers(structure(list(), class = "Date")),
-    skim_type = "difftime"
-  )
+  modify_default_skimmers("Date", new_skim_type = "difftime")
 }
 
 #' @export
@@ -204,38 +209,89 @@ get_skimmers.list <- function(column) {
 
 #' @export
 get_skimmers.AsIs <- function(column) {
-  purrr::list_modify(get_skimmers(list()), skim_type = "AsIs")
+  modify_default_skimmers("list", new_skim_type = "AsIs")
 }
 
 #' @rdname get_skimmers
-#' @param class The class of the column being skimmed
+#' @param new_skim_type The type to assign to the looked up set of skimmers.
+#' @param new_funs Replacement functions for those in
 #' @export
-get_default_skimmers <- function(class = NULL) {
-  if (is.null(class)) {
-    defaults <- as.character(utils::methods("get_skimmers"))
-    classes <- stringr::str_replace(defaults, "get_skimmers.", "")
-    no_default <- purrr::discard(classes, ~ .x == "default")
-    iter <- purrr::set_names(no_default)
-    purrr::map(iter, get_class_defaults)
-  } else {
-    iter <- purrr::set_names(class)
-    results <- purrr::map(iter, get_class_defaults)
-    no_defaults_for_class <- purrr::map_lgl(results, anyNA)
-    if (any(no_defaults_for_class)) {
-      dropped <- paste(results[no_defaults_for_class], collapse = ", ")
-      msg <- sprintf("The following classes do not have defaults: %s", dropped)
-      warning(msg, call. = FALSE)
-    }
-    results[!no_defaults_for_class]
-  }
+modify_default_skimmers <- function(
+    skim_type, new_skim_type = NULL, new_funs = list()) {
+  funs <- get_one_default_skimmer(skim_type)
+  new_funs <- purrr::list_modify(funs, !!!new_funs)
+  sfl(skim_type = new_skim_type %||% skim_type, !!!new_funs)
 }
 
-get_class_defaults <- function(class) {
-  skimmers <- get_skimmers(structure(integer(), class = class))
-
-  if (skimmers$skim_type == "default") {
-    NA
+#' View default skimmer names and functions
+#' 
+#' These utility functions look up the currently-available defaults for one or
+#' more `skim_type`'s. They work with all defaults in the `skimr` package, as
+#' well as the defaults in any package that extends `skimr`. See
+#' [get_skimmers()] for writing lookup methods for different.
+#' 
+#' The functions differ in return type and whether or not the result is in
+#' a list. `get_default_skimmers()` and `get_one_default_skimmer()` return
+#' functions. The former returns functions within a typed list, i.e.
+#' `list(numeric = list(...functions...))`.
+#' 
+#' The functions differ in return type and whether or not the result is in
+#' a list. `get_default_skimmer_names()` and `get_one_default_skimmer_names()` 
+#' return a list of character vectors or a single character vector.
+#' 
+#' `get_sfl` returns the skimmer function list for a particular `skim_type`.
+#' It differs from `get_default_skimmers` in that the returned `sfl` contains
+#' a list of functions and a `skim_type`.
+#' 
+#' @param skim_type The class of the column being skimmed.
+#' @export
+get_default_skimmers <- function(skim_type = NULL) {
+  if (is.null(skim_type)) {
+    defaults <- as.character(utils::methods("get_skimmers"))
+    types <- stringr::str_replace(defaults, "get_skimmers.", "")
+    no_default <- purrr::discard(types, ~ .x == "default")
+    iter <- purrr::set_names(no_default)
   } else {
-    names(skimmers$funs)
+    iter <- purrr::set_names(skim_type)
+  }
+  
+  results <- purrr::map(iter, get_one_default_skimmer)
+  no_defaults_for_class <- purrr::map_lgl(results, anyNA)
+  results[!no_defaults_for_class]
+}
+
+#' @describeIn get_default_skimmers Get the functions associated with one
+#'   `skim_type`.
+#' @export
+get_one_default_skimmer <- function(skim_type) {
+  skimmers <- get_sfl(skim_type)
+  skimmers$funs
+}
+
+#' @describeIn get_default_skimmers Get the names of the functions used in one
+#'   or more `skim_type`'s.
+#' @export
+get_default_skimmer_names <- function(skim_type = NULL) {
+  skimmers <- get_default_skimmers(skim_type)
+  purrr::map(skimmers, names)
+}
+
+#' @describeIn get_default_skimmers Get the names of the functions used in one
+#'   `skim_type`.
+#' @export
+get_one_default_skimmer_names <- function(skim_type) {
+  skimmers <- get_one_default_skimmer(skim_type)
+  names(skimmers)
+}
+
+#' @describeIn get_default_skimmers Get the `sfl` for a `skim_type`.
+#' @export
+get_sfl <- function(skim_type) {
+  skimmers <- get_skimmers(structure(integer(), class = skim_type))
+  if (skimmers$skim_type == "default") {
+    warning("There are no default skimmers for type: ", skim_type)
+    sfl(NA, skim_type = NA_character_)
+  } else {
+    skimmers
   }
 }
