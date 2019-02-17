fa68567d0b66528c9d82f7f6c806a594d0acdf80
Elin Waring <elin.waring@gmail.com>
2019-02-17 11:21:08 -0500

    Windows and others handle multibyte differently.

--- tests/testthat/test-stats.R
+++ tests/testthat/test-stats.R
@@ -121,11 +121,12 @@ test_that("min_char is calculated correctly, including empty strings.", {
   expect_identical(input, correct)
 })
 
-test_that("max_char with a multibyte character does not throw an error.", {
+test_that("min_char with a multibyte character does not throw an error.", {
   data <- c("a", "ab", "abc", "Coleophora asteris M\x9fhl.")
-  correct <- as.integer(1)
-  input <- min_char(data)
-  expect_identical(input, correct)
+  #correct <- as.integer(1)
+  #input <- min_char(data)
+  #expect_identical(input, correct)
+  expect_error(min_char(data), NA)
 })
 
 test_that("max_char is calculated correctly, including empty strings.", {
@@ -137,9 +138,10 @@ test_that("max_char is calculated correctly, including empty strings.", {
 
 test_that("max_char with a multibyte character does not throw an error.", {
   data <- c("a", "ab", "abc", "Coleophora asteris M\x9fhl.")
-  correct <- as.integer(3)
-  input <- max_char(data)
-  expect_identical(input, correct)
+  #correct <- as.integer(3)
+  #input <- max_char(data)
+  #expect_identical(input, correct)
+  expect_error(max_char(data, NA))
 })
 
 test_that("n_unique is calculated correctly.", {
