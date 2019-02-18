3425ca8eb33fc9119bac55d94bb46f29b499fbe7
Michael Quinn <michael.quinn@aya.yale.edu>
2019-02-17 20:21:02 -0800

    Refractor print tests to use reference files.
    This makes it easier to read the test and see all of the output.

--- tests/testthat/helper-expectations.R
+++ tests/testthat/helper-expectations.R
@@ -24,3 +24,41 @@ expect_NA <- function(object) {
   expect(is.na(act$val), sprintf("%s is not NA", act$lab))
   invisible(act$val)
 }
+
+expect_print_matches_file <- function(object, filename) {
+  withr::with_options(list(crayon.enabled = FALSE), {
+    testthat::expect_known_output(
+      print(object),
+      filename,
+      update = TRUE,
+      width = 100
+    )
+  })
+}
+
+expect_matches_file <- function(object, file, update = TRUE, ...) {
+  act <- testthat::quasi_label(rlang::enquo(object), NULL)
+
+  if (!file.exists(file)) {
+    warning("Creating reference value", call. = FALSE)
+    writeLines(object, file)
+    testthat::succeed()
+  } else {
+    ref_val <- paste0(readLines(file), collapse = "\n")
+    comp <- testthat::compare(as.character(act$val), ref_val, ...)
+    if (update && !comp$equal) {
+      writeLines(act$val, file)
+    }
+
+    expect(
+      comp$equal,
+      sprintf(
+        "%s has changed from known value recorded in %s.\n%s",
+        act$lab, encodeString(file, quote = "'"), comp$message
+      ),
+      info = NULL
+    )
+  }
+
+  invisible(act$value)
+}
--- tests/testthat/test-dplyr.R
+++ tests/testthat/test-dplyr.R
@@ -4,38 +4,34 @@ skimmed_iris <- skim(iris)
 
 test_that("dplyr::filter works as expected", {
   input <- dplyr::filter(skimmed_iris, skim_type == "numeric")
-  expect_output(print(input), "Data summary  \\n")
+  expect_print_matches_file(input, "dplyr/filter-skim.txt")
 
   no_rows <- dplyr::filter(skimmed_iris, skim_type == "no_type")
-  expect_output(print(no_rows), "# A tibble: 0 x 16")
+  expect_print_matches_file(input, "dplyr/filter-no-skim.txt")
 })
 
 test_that("dplyr::select works as expected", {
   with_type <- dplyr::select(skimmed_iris, skim_type, skim_variable)
-  expect_output(print(with_type), "Data summary  \\n")
+  expect_print_matches_file(with_type, "dplyr/select-skim.txt")
 
   without_type <- dplyr::select(skimmed_iris, mean)
-  expect_output(print(without_type), "# A tibble")
+  expect_print_matches_file(without_type, "dplyr/select-no-skim.txt")
 })
 
 test_that("dplyr::mutate works as expected", {
   input <- dplyr::mutate(skimmed_iris, mean2 = mean^2)
-  expect_output(print(input), "Data summary  \\n")
+  expect_print_matches_file(input, "dplyr/mutate-skim.txt")
 
   no_variable <- dplyr::mutate(skimmed_iris, skim_variable = NULL)
-  expect_output(print(no_variable), "# A tibble: 5 x 15")
+  expect_print_matches_file(no_variable, "dplyr/mutate-no-skim.txt")
 })
 
 test_that("dplyr::slice works as expected", {
   input <- dplyr::slice(skimmed_iris, 1:3)
-  expect_output(print(input), "Data summary  \\n")
+  expect_print_matches_file(input, "dplyr/slice.txt")
 })
 
 test_that("dplyr::arrange works as expected", {
   input <- dplyr::arrange(skimmed_iris, desc(mean))
-  expect_output(print(input), "Data summary  \\n")
-  expect_output(print(input), "1 Sepal.Length")
-  expect_output(print(input), "2 Petal.Length")
-  expect_output(print(input), "3 Sepal.Width")
-  expect_output(print(input), "4 Petal.Width")
+  expect_print_matches_file(input, "dplyr/arrange.txt")
 })
