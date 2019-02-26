573ae6d3724edcdc5ebeaa96e59ef4abf9169d53
Michael Quinn <michael.quinn@aya.yale.edu>
2019-02-25 18:11:46 -0800

    Don't rely on the option.

--- R/skim_print.R
+++ R/skim_print.R
@@ -39,7 +39,7 @@ print.one_skim_df <- function(x, n = Inf, width = Inf, n_extra = NULL, ...) {
 }
 
 grab_tibble_metadata <- function(x) {
-  if (rlang::is_true(getOption("crayon.enabled"))) {
+  if (crayon::has_color()) {
     grep("^\\\033\\[38;5;\\d{3}m[#\\*]", x)
   } else {
     grep("^[#\\*]", x)
