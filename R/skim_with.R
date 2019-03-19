db88fee1af0d9f9ba772eb40c3ac9d63b4ba678a
Elin Waring <elin.waring@gmail.com>
2019-03-19 06:09:24 -0400

    skim_tee() should respect ... options.

--- R/skim_with.R
+++ R/skim_with.R
@@ -284,7 +284,7 @@ build_results <- function(skimmed, data, data_cols, groups, new_names, delim) {
   } else {
     tibble::tibble(
       skim_variable = data_cols,
-      !!!purrr::set_names(skimmed, new_names)
+      names(skimmed) <- new_names
     )
   }
 }
