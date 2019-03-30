67f6441c0d4bcc89dec9b0bb44c55a9b84324fde
Michael Quinn <michael.quinn@aya.yale.edu>
2019-03-30 13:55:09 -0700

    Refactor skim_tee tests

---
+++ tests/testthat/test-skim_tee.R
@@ -0,0 +1,26 @@
+context("Using skim_tee")
+
+test_that("Using skim_tee prints returns the object", {
+  expect_known_output(
+    skim_object <- skim_tee(chickwts), "skim_tee/skim_tee.txt")
+  expect_identical(chickwts, skim_object)
+})
+
+test_that("skim_tee prints only selected columns, but returns full object", {
+  expect_known_output(
+    obj <- skim_tee(iris, Species), "skim_tee/species.txt")
+  expect_identical(obj, iris)
+})
+
+test_that("skim_tee supports dplyr helpers", {
+  expect_known_output(
+    obj <- skim_tee(iris, starts_with("Sepal")), "skim_tee/sepal.txt")
+  expect_identical(obj, iris)
+})
+
+test_that("Skim_tee works with groups", {
+  iris_grouped <- dplyr::group_by(iris, Species)
+  expect_known_output(
+    obj <- skim_tee(iris_grouped, Sepal.Length), "skim_tee/grouped.txt")
+  expect_identical(obj, iris_grouped)
+})
