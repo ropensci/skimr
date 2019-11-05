16cc90fa53d10229ebba69c7f20b44f0e039d96f
Michael Quinn <msquinn@google.com>
2019-11-04 21:40:58 -0500

    Fixing warning message

--- R/skim_with.R
+++ R/skim_with.R
@@ -194,8 +194,12 @@ get_final_skimmers <- function(column, data, local_skimmers, append) {
 
   if (is.null(locals$funs)) {
     if (defaults$skim_type == "default") {
-      warning("Couldn't find skimmers for class: %s; No user-defined `sfl` ",
-        "provided. Falling back to `character`.",
+      msg <- sprintf(
+        "Couldn't find skimmers for class: %s;",
+        paste(all_classes, collapse = ", ")
+      )
+      warning(msg,
+        " No user-defined `sfl` provided. Falling back to `character`.",
         call. = FALSE
       )
       data[[column]] <- as.character(data[[column]])
