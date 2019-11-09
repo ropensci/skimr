78ad67f8f9b59d4f90fc0588f0d347b849f9ec55
Elin Waring <elin.waring@gmail.com>
2019-11-09 10:08:31 -0500

    Fixing some documentation notes from check_for_cran().

--- R/dplyr.R
+++ R/dplyr.R
@@ -1,25 +1,27 @@
 #' Mutate a skim_df
 #'
-#' [dplyr::mutate()] currently drops attributes, but we need to keep them around
+#' \code{\link[dplyr]{mutate}} currently drops attributes, but we need to keep them around
 #' for other skim behaviors. Otherwise the behavior is exactly the same. For
-#' more information, see https://github.com/tidyverse/dplyr/issues/3429.
+#' more information, see \url{https://github.com/tidyverse/dplyr/issues/3429}.
 #'
 #' @param .data A `skim_df`, which behaves like a `tbl.`
 #' @param ... Name-value pairs of expressions, each with length 1 or the same
-#'   length as the number of rows in the group (if using [dplyr::group_by()] or
+#'   length as the number of rows in the group, if using [dplyr::group_by()], or
 #'   in the entire input (if not using groups). The name of each argument will
 #'   be the name of a new variable, and the value will be its corresponding
 #'   value. Use `NULL` value in `mutate` to drop a variable. New variables
 #'   overwrite existing variables of the same name.
 #'
-#'   The arguments in `...` are automatically [quoted][rlang::quo] and
-#'   [evaluated][rlang::eval_tidy] in the context of the data frame. They
-#'   support [unquoting][rlang::quasiquotation] and splicing. See
+#'   The arguments in `...` are automatically quoted with
+#'  \code{\link[rlang]{quo}} and evaluated with \code{\link[rlang]{eval_tidy}} 
+#'   in the context of the data frame. They
+#'   support unquoting \code{\link[rlang]{quasiquotation}} and splicing. See
 #'   `vignette("programming", package = "dplyr")` for an introduction to these
 #'   concepts.
 #' @return A `skim_df` object, which also inherits the class(es) of the input
-#'   data. In many ways, the object behaves like a [tibble::tibble()].
-#' @seealso [dplyr::mutate()] for the function's expected behavior.
+#'   data. In many ways, the object behaves like a 
+#'   \code{\link[tibble]{tibble}}.
+#' @seealso \code{\link[dplyr]{mutate}} for the function's expected behavior.
 #' @importFrom dplyr mutate
 #' @export
 mutate.skim_df <- function(.data, ...) {
