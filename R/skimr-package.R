cf1f1aaca8766a1f7c36a4c56549b99e524bf464
olivroy <olivierroy71@hotmail.com>
2025-11-19 09:19:36 -0500

    Add back re-exports removed in 45aeeaa1ba286cd3d63ba66013942029fc9f774c to make sure it is possible to use tidyselect helpers without dplyr loaded.

--- R/skimr-package.R
+++ R/skimr-package.R
@@ -5,3 +5,36 @@
 #' @importFrom rlang %||% .data
 ## usethis namespace: end
 NULL
+# Imports -----------------------------------------------------------------
+
+#' @importFrom tidyselect contains
+#' @export
+tidyselect::contains
+
+#' @importFrom tidyselect ends_with
+#' @export
+tidyselect::ends_with
+
+#' @importFrom tidyselect everything
+#' @export
+tidyselect::everything
+
+#' @importFrom tidyselect matches
+#' @export
+tidyselect::matches
+
+#' @importFrom tidyselect num_range
+#' @export
+tidyselect::num_range
+
+#' @importFrom tidyselect all_of
+#' @export
+tidyselect::all_of
+
+#' @importFrom tidyselect any_of
+#' @export
+tidyselect::any_of
+
+#' @importFrom tidyselect starts_with
+#' @export
+tidyselect::starts_with
